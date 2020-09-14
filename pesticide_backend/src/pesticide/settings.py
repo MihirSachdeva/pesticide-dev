7067b217f4dd62d02882c4261444a47b0140b645
MihirSachdeva <mihirsachdeva.ms@gmail.com>
2020-09-14 19:54:17 +0530

    Whitelist FRONTEND_URL in settings.py

--- pesticide_backend/src/pesticide/settings.py
+++ pesticide_backend/src/pesticide/settings.py
@@ -154,10 +154,7 @@ OAUTH2_PROVIDER = {
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = (
-    'http://localhost:3000',
-    'http://localhost:8000',
-    "http://127.0.0.1:3000",
-    "http://0.0.0.0:3000"
+    FRONTEND_URL,
 )
 SITE_ID = 1
 
